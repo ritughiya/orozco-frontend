1059d8e36a422df3ad53e570146e7eed385cf355
ritughiya <ritughiya@gmail.com>
2022-02-03 09:43:18 -0800

    logo change

--- components/Logo.js
+++ components/Logo.js
@@ -3,21 +3,18 @@ import React from "react";
 export default function Logo() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 998 1003.38">
-  <g id="Layer_2" data-name="Layer 2">
-    <g id="Layer_1-2" data-name="Layer 1">
-      <path className="cls-1" d="M499,242.5V499H242.5C242.5,357.34,357.34,242.5,499,242.5Z"/>
-      <path className="cls-2" d="M499,499V755.5c-141.66,0-256.5-114.84-256.5-256.5Z"/>
-      <path className="cls-3" d="M755.5,499H499V242.5C640.66,242.5,755.5,357.34,755.5,499Z"/>
-      <path className="cls-2" d="M998,499H755.5A121.25,121.25,0,0,1,998,499Z"/>
-      <path className="cls-2" d="M499,242.5V0a121.25,121.25,0,0,1,0,242.5Z"/>
-      <path className="cls-1" d="M755.5,499H998a121.25,121.25,0,0,1-242.5,0Z"/>
-      <path className="cls-3" d="M499,760.88v242.5a121.25,121.25,0,0,1,0-242.5Z"/>
-      <path className="cls-1" d="M499,1003.38V760.88a121.25,121.25,0,1,1,0,242.5Z"/>
-      <path className="cls-3" d="M0,499H242.5A121.25,121.25,0,0,1,0,499Z"/>
-      <path className="cls-1" d="M755.5,499c0,141.66-114.84,256.5-256.5,256.5V499Z"/>
-      <line className="cls-4" x1="148.5" y1="500.5" x2="106" y2="500.5"/>
-    </g>
-  </g>
+  <g id="Layer_2_00000029020245000934518380000011440708550443110795_">
+	<g id="Layer_1-2">
+		<path className="st0" d="M499,499v256.5c-141.7,0-256.5-114.8-256.5-256.5H499z"/>
+		<path className="st1" d="M755.5,499H499V242.5C640.7,242.5,755.5,357.3,755.5,499z"/>
+		<path className="st0" d="M998,499H755.5c0-67,54.3-121.2,121.2-121.2S998,432,998,499z"/>
+		<path className="st0" d="M499,242.5V0c67,0,121.2,54.3,121.2,121.2S566,242.5,499,242.5z"/>
+		<path className="st1" d="M499,760.9v242.5c-67,0-121.2-54.3-121.2-121.2S432,760.9,499,760.9z"/>
+		<path className="st1" d="M0,499h242.5c0,67-54.3,121.2-121.3,121.2S0,566,0,499z"/>
+		<path className="st2" d="M755.5,499c0,141.7-114.8,256.5-256.5,256.5V499H755.5z"/>
+		<line className="st3" x1="148.5" y1="500.5" x2="106" y2="500.5"/>
+	</g>
+</g>
 </svg>
 
   );
