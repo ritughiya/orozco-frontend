8330ddb308e5eaecddb8d2115bc7b762042a9106
ritughiya <ritughiya@gmail.com>
2022-03-27 17:59:22 -0400

    carche2

--- pages/index.js
+++ pages/index.js
@@ -99,50 +99,6 @@ const IndexPage = ({ properties }) => {
         }
       })()}
 
-
-      {/* {properties.map(post => (
-        <div key={post._id}>
-              
-        
-      <If condition={ count === "1"}>
-      <div className="vimeo-wrapper pointer">
-              <iframe loading="lazy" src="https://player.vimeo.com/video/675559033?h=8b71bc1ada&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>
-          </div>
-
-          <Animation3 /> 
-          <Logotransparent />
-           <Ticker />
-           <Label />
-           <Linkwrapper />   
-
-          <div className="mainContainer">
-            {count}ONE
-            <Container1 />
-            <Container2 />
-            <Container3 />
-            <Container4 />
-           </div> 
-           <Footerdelay />
-      <Else>
-            <Quicklogo />
-            <Quickticker />
-            <Quicklabel />
-            <Quicklinkwrapper />   
-            <div className="mainContainer">
-            {count}TWO
-            <Homecontainer1 />
-            <Homecontainer2 />
-            <Homecontainer3 />
-            <Homecontainer4 />
-           </div> 
-           <Footer />
-        </Else>
-
-      </If>
-
-        </div>
-        
-      ))} */}
       
     </div>
   )
