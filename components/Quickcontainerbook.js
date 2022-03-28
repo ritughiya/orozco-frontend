d39fae95cbd844787ab1759ea3b4d70c3f4986f0
ritughiya <ritughiya@gmail.com>
2022-03-28 17:54:03 -0400

    custom arrows

--- components/Quickcontainerbook.js
+++ components/Quickcontainerbook.js
@@ -12,12 +12,14 @@
  
  export default function Quickcontainerbook() {
    return (
-    <div className=" pointer v1">
+     <div>
+     <div id="bookoverlay"></div>
+    <div className="  v1">
                   
       {/* <div className="embed-container">
       <iframe loading="lazy" className="secondaryView" src="https://player.vimeo.com/video/680920205?h=32ecfe3749&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>  
      </div> */}
-     <div className="embed-container">
+     <div className="embed-container zoomcursor">
      <RoomLabel />
     <Roomsdropdown2 />
      <iframe loading="lazy" className="" src="https://player.vimeo.com/video/683943451?h=ae18c31333&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>   
@@ -26,7 +28,7 @@
       {/* <div className="camTopleft primaryCamtitle">Cam1</div> */}
      </div> 
     
- 
+     </div>
  
    );
  }
