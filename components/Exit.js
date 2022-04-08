d8eba02556abcdff4e87fc7ed7cb0873be0be5cc
ritughiya <ritughiya@gmail.com>
2022-04-08 13:05:44 -0400

    rebuild

--- components/Exit.js
+++ components/Exit.js
@@ -4,8 +4,8 @@ import React from "react";
 export default function Exit() {
   return (
 <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2 2L29 29" stroke="black" stroke-width="4.42"/>
-<path d="M29 2L2 29" stroke="black" stroke-width="4.42"/>
+<path d="M2 2L29 29" stroke="black" strokeWidth="4.42"/>
+<path d="M29 2L2 29" stroke="black" strokeWidth="4.42"/>
 </svg>
 
 
