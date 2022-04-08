68e2027c09292901dea31107e650fa41627939b5
ritughiya <ritughiya@gmail.com>
2022-04-08 13:02:51 -0400

    reset exit

--- components/Reset.js
+++ components/Reset.js
@@ -11,10 +11,7 @@ import Script from 'next/script'
 import { sanityClient, urlFor} from '../sanity'
  import Clock from 'react-live-clock';
  import Caret from "../components/Caret"
- import Room1thumbnail from "../public/room1.png"
- import Room2thumbnail from "../public/room2.png"
- import Room3thumbnail from "../public/room3.png"
- import Room4thumbnail from "../public/room4.png"
+ import Exit from "../components/Exit"
 
 
 import ReactDOM from 'react-dom';
@@ -27,7 +24,7 @@ const Reset = () => {
      
      <ul className="dropdowncontainer Resetbutton">
      <li>
-         <Link href="/archive">Reset X </Link>
+         <Link href="/archive"><span className="resetspan">Reset <Exit /> </span></Link>
          
      </li>
  </ul>
