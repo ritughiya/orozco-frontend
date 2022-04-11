c8cd9c18323a94f9e936f2eb77422c34e6a4a38f
ritughiya <ritughiya@gmail.com>
2022-04-11 11:33:36 -0400

    label

--- components/Quickcontainerbook.js
+++ components/Quickcontainerbook.js
@@ -20,7 +20,7 @@
       <iframe loading="lazy" className="secondaryView" src="https://player.vimeo.com/video/680920205?h=32ecfe3749&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>  
      </div> */}
      <div className="embed-container zoomcursor">
-     <Room1Label />
+     <BookshelfLabel />
     <Roomsdropdown2 />
      <iframe loading="lazy" className="" src="https://player.vimeo.com/video/683943451?h=ae18c31333&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>   
      </div>
