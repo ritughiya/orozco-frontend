4e8d0192b029fa3cb0636a82db024a7cb202d88b
ritughiya <ritughiya@gmail.com>
2022-04-14 15:47:04 -0400

    width

--- pages/work/[slug].jsx
+++ pages/work/[slug].jsx
@@ -385,9 +385,9 @@ const Work = ({
           return (
             <>
             {relatedworks && relatedworks.map(({_id, mainImage = '', caption = '', year = '', medium = '', dimensions = '', editionDetails =''}) => (
-         <div key={_id}> <div className="workposition" style={{ position: 'relative',  width: '90vw', height: '65vh' }}>
+         <div key={_id}> <div className="workposition" style={{ position: 'relative',  width: '92vw' }}>
             
-          <Image className="placeholder" draggable="false" onDragStart="return false;" src={urlFor(mainImage).url()} width="100%" height="100%" layout="fill" objectFit="contain" placeholder="blur"
+          <Image className="placeholder" draggable="false" onDragStart="return false;" src={urlFor(mainImage).url()} width="100%" height="100%" layout="responsive" objectFit="contain" placeholder="blur"
       blurDataURL={rgbDataURL(73, 71, 63)}/></div>
                     <div className="info">
                 {caption}, {year}<br></br>
