b5d34ea05c7d1bc785d98b1c253beb267c867f7e
ritughiya <ritughiya@gmail.com>
2022-04-14 15:17:00 -0400

    mobile item

--- pages/work/[slug].jsx
+++ pages/work/[slug].jsx
@@ -356,9 +356,9 @@ const Work = ({
       <div className="singlecontainer mobile">
       <div className="workcontainer">
       {relatedworks && relatedworks.map(({_id, mainImage = '', caption = '', year = '', medium = '', dimensions = '', editionDetails =''}) => (
-         <div key={_id}> <div className="workposition" style={{ position: 'relative',  width: '90vw', height: '100%' }}>
+         <div key={_id}> <div className="workposition" style={{ position: 'relative',  width: '88vw', height: '69vh' }}>
             
-          <Image className="placeholder" draggable="false" onDragStart="return false;" src={urlFor(mainImage).url()} width="100%" height="100%" layout="responsive" objectFit="contain" placeholder="blur"
+          <Image className="placeholder" draggable="false" onDragStart="return false;" src={urlFor(mainImage).url()} width="100%" height="100%" layout="fill" objectFit="contain" placeholder="blur"
       blurDataURL={rgbDataURL(73, 71, 63)}/></div>
                     <div className="info">
                 {caption}, {year}<br></br>
