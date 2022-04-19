ec07b15ca403a58fd8716a5d94d138b957366646
ritughiya <ritughiya@gmail.com>
2022-04-19 09:36:27 -0400

    fix

--- components/Homecontainer2mobile.js
+++ components/Homecontainer2mobile.js
@@ -17,10 +17,10 @@
   <div>
 
  <div className="camTopright ">Cam2</div>
- <a href="/room2" className="linkwrap">
+ {/* <a href="/room2" className="linkwrap"> */}
  <div className="blocker"></div>
  <iframe className=" responsive-iframe" src="https://player.vimeo.com/video/699610145?h=cf8c139cdd&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="100%" height="100vh" frameBorder="0" allowFullScreen></iframe>   
-  </a>
+  {/* </a> */}
 
   </div>
 
