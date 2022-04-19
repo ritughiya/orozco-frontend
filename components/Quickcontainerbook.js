3fe479f877860ea51e2de807f0d3ff925d35864f
ritughiya <ritughiya@gmail.com>
2022-04-19 16:00:16 -0400

    get rid of hor scroll

--- components/Quickcontainerbook.js
+++ components/Quickcontainerbook.js
@@ -5,27 +5,51 @@
  */
 
  import React from "react";
+ import Image from 'next/image'
  import BookshelfLabel from "../components/BookshelfLabel"
  import Roomsdropdown from "../components/Roomsdropdown"
  import Roomsdropdown2 from "../components/Roomsdropdown2"
+ import Bookshelf  from "../public/bookshelf.png"
 
+const keyStr =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
+const triplet = (e1, e2, e3) =>
+  keyStr.charAt(e1 >> 2) +
+  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+  keyStr.charAt(e3 & 63)
+
+const rgbDataURL = (r, g, b) =>
+  `data:image/gif;base64,R0lGODlhAQABAPAA${
+    triplet(0, r, g) + triplet(b, 255, 255)
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
+
+  
+
+ 
  
  export default function Quickcontainerbook() {
    return (
+
+    
      <div>
-     {/* <div id="bookoverlay"></div> */}
+
+<div style={{ position: 'relative', width: '80vh', height: '100%' }}>
+    <Image className="placeholder" draggable="false" onDragStart="return false;" onContextMenu={(e)=> e.preventDefault()} src="/public/bookshelf.png" width="100%" height="100%" layout="fill" placeholder="blur"
+    blurDataURL={rgbDataURL(73, 71, 63)}/>
+    </div>
+    
     <div className="  v1">
                   
-      {/* <div className="embed-container">
-      <iframe loading="lazy" className="secondaryView" src="https://player.vimeo.com/video/680920205?h=32ecfe3749&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>  
-     </div> */}
-     <div className="embed-container zoomcursor">
+     
+     <div className="embed-container ">
      <BookshelfLabel />
     <Roomsdropdown2 />
-     <iframe loading="lazy" className="" src="https://player.vimeo.com/video/683943451?h=ae18c31333&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>   
+   
+            
      </div>
-    {/* <div className="camTopleft secondaryCamtitle">Cam7</div> */}
-      {/* <div className="camTopleft primaryCamtitle">Cam1</div> */}
+    
      </div> 
     
      </div>
