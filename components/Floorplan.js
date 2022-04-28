1f9031edcfa785a42b8b459b6f95e9915d0c0aea
ritughiya <ritughiya@gmail.com>
2022-04-28 11:13:34 -0400

    synxtax

--- components/Floorplan.js
+++ components/Floorplan.js
@@ -28,15 +28,15 @@ export default function Floorplan() {
     <line className="i" x1="525.34" y1="252.08" x2="525.34" y2="312.11" />
     <line className="i" x1="525.34" y1="155.97" x2="525.34" y2="215.7" />
     <line className="g" x1="336.13" y1="142.57" x2="336.13" y2="201.15" />
-    <line class="i" x1="558.28" y1="120.39" x2="558.28" y2="155.78"/>
-    <line class="i line" x1="255.62" y1="142.57" x2="255.62" y2="145.45"/>
+    <line className="i" x1="558.28" y1="120.39" x2="558.28" y2="155.78"/>
+    <line className="i line" x1="255.62" y1="142.57" x2="255.62" y2="145.45"/>
 
-    <polyline class="i cls-14" points="154.84 142.99 229.91 142.99 229.91 145.45"/>
+    <polyline className="i cls-14" points="154.84 142.99 229.91 142.99 229.91 145.45"/>
   
     <g id="spaceship">
     <a xlinkHref="/spaceshiproom"  target="_blank">
     <rect className="m1" x="245.43" y="27.9" width="96.08" height="81.44" />
-    <text className="maptext m1text" x="266" y="62" font-family="abcSimon" font-size="0.6em" >Spaceship
+    <text className="maptext m1text" x="266" y="62" fontFamily="abcSimon" fontSize="0.6em" >Spaceship
         <tspan y="75" x="279">Room</tspan></text>
         </a>
 
@@ -44,8 +44,8 @@ export default function Floorplan() {
     <g id="offices">
     <a xlinkHref="/offices"  target="_blank">
     <rect className="m2" x="154.84" y="144.92" width="181.19" height="55.8" />
-    <line class="i line" x1="255.62" y1="145.45" x2="255.62" y2="185.7"/>
-    <polyline class="i line" points="336.02 185.75 229.91 185.75 229.91 145.45"/>
+    <line className="i line" x1="255.62" y1="145.45" x2="255.62" y2="185.7"/>
+    <polyline className="i line" points="336.02 185.75 229.91 185.75 229.91 145.45"/>
     <line className="i line" x1="229.65" y1="170.22" x2="255.62" y2="170.22" />
     <line className="i line" x1="229.65" y1="164.86" x2="255.62" y2="164.86" />
     <line className="i line" x1="229.65" y1="180.93" x2="255.62" y2="180.93" />
@@ -55,7 +55,7 @@ export default function Floorplan() {
     <line className="i line" x1="229.65" y1="154.15" x2="255.62" y2="154.15" />
     <line className="i line" x1="229.65" y1="148.8" x2="255.62" y2="148.8" />
 
-    <text className="maptext m2text" x="170" y="170" font-family="abcSimon" font-size="0.6em" >Offices</text>
+    <text className="maptext m2text" x="170" y="170" fontFamily="abcSimon" fontSize="0.6em" >Offices</text>
         </a>
 
     </g>
@@ -65,27 +65,27 @@ export default function Floorplan() {
     <rect className="i object" x="373.33" y="254.54" width="30.46" height="42.07" />
     <rect className="i object" x="403.78" y="179.08" width="30.46" height="15.39" />
     <rect className="i object" x="475.24" y="316.1" width="5.44" height="26.19" />
-    <text className="maptext m3text" x="375" y="250" font-family="abcSimon" font-size="0.6em" >Room One</text>
+    <text className="maptext m3text" x="375" y="250" fontFamily="abcSimon" fontSize="0.6em" >Room One</text>
     </a>
     </g>
     <g id="room2">
     <a xlinkHref="/room2"  target="_blank">
     <rect className="m4" x="367.07" y="24.49" width="216.34" height="95.84" />
     <circle className="i object" cx="544.69" cy="65.79" r="6.1" />
-    <polyline class="i object" points="388.29 120.64 388.29 55.88 558.28 55.88 558.28 120.39"/>
+    <polyline className="i object" points="388.29 120.64 388.29 55.88 558.28 55.88 558.28 120.39"/>
     <rect className="i object" x="409.44" y="55.88" width="118.77" height="11.91" />
-    <text className="maptext m4text" x="445" y="85" font-family="abcSimon" font-size="0.6em" >Room Two</text>
+    <text className="maptext m4text" x="445" y="85" fontFamily="abcSimon" fontSize="0.6em" >Room Two</text>
     </a></g>
     <g id="room3">
     <a xlinkHref="/room3" target="_blank">
-    <rect class="m5" x="357" y="120.66" width="103" height="49.03" />
+    <rect className="m5" x="357" y="120.66" width="103" height="49.03" />
     <rect className="i table" x="367.07" y="157.79" width="84.75" height="11.91" />
     <rect className="i table" x="367.07" y="120.96" width="84.75" height="11.91" />
-    <text className="maptext m5text" x="377" y="148" font-family="abcSimon" font-size="0.6em" >Room Three</text>
+    <text className="maptext m5text" x="377" y="148" fontFamily="abcSimon" fontSize="0.6em" >Room Three</text>
     </a></g>
 
-    <path class="chess" d="M200.56,58.34a2.82,2.82,0,0,0,2.81-2.81c0-1.55-1.14-2.6-2.81-3.19a4.28,4.28,0,0,1-2.39-2c-.52-.9-1.65-2.28-3.16-2.28H193V46.28c-11.11,4.16-7.51,19.12-7.51,19.12h2.22v1.46H187a.88.88,0,0,0-.89.83v.1a.81.81,0,0,0,.15.46.85.85,0,0,0,.38.3,3.54,3.54,0,0,0,.08,3.5h-.55a.88.88,0,0,0-.89.83v1.87a.85.85,0,0,0,.83.83v1.67h13.71V75.58a.86.86,0,0,0,.52-.28.79.79,0,0,0,.21-.55V72.88a.78.78,0,0,0-.77-.83h-.44a3.56,3.56,0,0,0,0-3.5.81.81,0,0,0,.47-.76v-.1a.78.78,0,0,0-.76-.83h-.8V65.4h2.51a16.69,16.69,0,0,0-6.89-8.62H198a2.83,2.83,0,0,0,1,1.13A2.8,2.8,0,0,0,200.56,58.34Z"/>
-  <path class="chess" d="M550,184a2.73,2.73,0,0,0,1-1.14h4.18a16.79,16.79,0,0,0-6.9,8.63h2.51V193H550a.78.78,0,0,0-.77.83v.11a.89.89,0,0,0,.12.45.76.76,0,0,0,.35.31,3.62,3.62,0,0,0,0,3.5h-.44a.79.79,0,0,0-.77.83v1.87a.86.86,0,0,0,.21.55.83.83,0,0,0,.52.27v1.67H563v-1.66a.92.92,0,0,0,.58-.25.9.9,0,0,0,.25-.58V199a.89.89,0,0,0-.9-.83h-.55a3.54,3.54,0,0,0,.08-3.5.81.81,0,0,0,.38-.3.88.88,0,0,0,.16-.46v-.11a.89.89,0,0,0-.9-.83h-.66v-1.45h2.22s3.6-15-7.52-19.12v1.77h-2.06c-1.5,0-2.64,1.38-3.16,2.27a4.26,4.26,0,0,1-2.39,2c-1.67.59-2.8,1.64-2.8,3.19a2.8,2.8,0,0,0,2.8,2.8A2.76,2.76,0,0,0,550,184Z"/>
+    <path className="chess" d="M200.56,58.34a2.82,2.82,0,0,0,2.81-2.81c0-1.55-1.14-2.6-2.81-3.19a4.28,4.28,0,0,1-2.39-2c-.52-.9-1.65-2.28-3.16-2.28H193V46.28c-11.11,4.16-7.51,19.12-7.51,19.12h2.22v1.46H187a.88.88,0,0,0-.89.83v.1a.81.81,0,0,0,.15.46.85.85,0,0,0,.38.3,3.54,3.54,0,0,0,.08,3.5h-.55a.88.88,0,0,0-.89.83v1.87a.85.85,0,0,0,.83.83v1.67h13.71V75.58a.86.86,0,0,0,.52-.28.79.79,0,0,0,.21-.55V72.88a.78.78,0,0,0-.77-.83h-.44a3.56,3.56,0,0,0,0-3.5.81.81,0,0,0,.47-.76v-.1a.78.78,0,0,0-.76-.83h-.8V65.4h2.51a16.69,16.69,0,0,0-6.89-8.62H198a2.83,2.83,0,0,0,1,1.13A2.8,2.8,0,0,0,200.56,58.34Z"/>
+  <path className="chess" d="M550,184a2.73,2.73,0,0,0,1-1.14h4.18a16.79,16.79,0,0,0-6.9,8.63h2.51V193H550a.78.78,0,0,0-.77.83v.11a.89.89,0,0,0,.12.45.76.76,0,0,0,.35.31,3.62,3.62,0,0,0,0,3.5h-.44a.79.79,0,0,0-.77.83v1.87a.86.86,0,0,0,.21.55.83.83,0,0,0,.52.27v1.67H563v-1.66a.92.92,0,0,0,.58-.25.9.9,0,0,0,.25-.58V199a.89.89,0,0,0-.9-.83h-.55a3.54,3.54,0,0,0,.08-3.5.81.81,0,0,0,.38-.3.88.88,0,0,0,.16-.46v-.11a.89.89,0,0,0-.9-.83h-.66v-1.45h2.22s3.6-15-7.52-19.12v1.77h-2.06c-1.5,0-2.64,1.38-3.16,2.27a4.26,4.26,0,0,1-2.39,2c-1.67.59-2.8,1.64-2.8,3.19a2.8,2.8,0,0,0,2.8,2.8A2.76,2.76,0,0,0,550,184Z"/>
 </svg>
 
 <div id="floorplantext">24 WEST 57TH, ROOM 305, NEW YORK NY 10019</div>
