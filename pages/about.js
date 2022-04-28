7a6c4ee7f05e619d2c20224a3a612b10882f053b
ritughiya <ritughiya@gmail.com>
2022-04-28 13:09:21 -0400

    syntax

--- pages/about.js
+++ pages/about.js
@@ -49,7 +49,7 @@ const serializers = {
       // Read https://css-tricks.com/use-target_blank/
       const { blank, href } = mark
       return blank ?
-        <a href={href} target="_blank" rel="noopener">{children}</a>
+        <a href={href} target="_blank" rel="noreferrer">{children}</a>
         : <a href={href}>{children}</a>
     }
   }
