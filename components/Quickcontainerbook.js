43281fba7883f9064485d3da323d4ddb54d9e329
ritughiya <ritughiya@gmail.com>
2022-05-02 08:16:42 -0400

    library

--- components/Quickcontainerbook.js
+++ components/Quickcontainerbook.js
@@ -34,17 +34,16 @@ const rgbDataURL = (r, g, b) =>
 
     
 
-    <div className="Bookshelfpage v1">
+    <div className="Bookshelfpage pointer v1">
                   
-     
-     <div className="embed-container ">
+      
+     <div className="embed-container">
      <BookshelfLabel />
     <Roomsdropdown2 />
-    <Image src={Bookshelf} />
-
-            
-     </div>
+    <iframe loading="lazy" className="" src="https://player.vimeo.com/video/699605384?h=edc6d07951&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>   
     
+     </div>
+
      </div> 
     
  
