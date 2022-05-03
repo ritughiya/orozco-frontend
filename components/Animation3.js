269d531a09608d0ec65ff5b074d1fd510caa8189
ritughiya <ritughiya@gmail.com>
2022-05-03 16:29:36 -0400

    opening animation

--- components/Animation3.js
+++ components/Animation3.js
@@ -3,27 +3,29 @@ import React from "react";
 export default function Animation3() {
   return (
 <div id="Orozcoanimation3">
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1938.01 1172.58">
-<g id="Layer_2" data-name="Layer 2">
-        <g id="Layer_1-2" data-name="Layer 1">
-            <circle className="cls-1" cx="625.14" cy="288.54" r="194.71" />
-            <circle className="cls-1" cx="1307.75" cy="288.54" r="194.71" />
-            <circle className="cls-2" cx="966.45" cy="107.13" r="107.13" />
-            <circle className="cls-2" cx="966.45" cy="250.05" r="33.85" />
-            <circle className="cls-2" cx="966.45" cy="1065.44" r="107.13" />
-            <circle className="cls-2" cx="966.45" cy="922.53" r="33.85" />
-            <circle className="cls-2" cx="1443.51" cy="590.39" r="107.13" />
-            <circle className="cls-2" cx="1300.6" cy="590.39" r="33.85" />
-            <circle className="cls-2" cx="496.56" cy="590.39" r="107.13" />
-            <circle className="cls-2" cx="639.48" cy="590.39" r="33.85" />
-            <circle className="cls-1" cx="1307.75" cy="906.47" r="194.71" />
-            <circle className="cls-1" cx="1743.3" cy="590.39" r="194.71" />
-            <circle className="cls-1" cx="194.71" cy="590.39" r="194.71" />
-            <circle className="cls-1" cx="625.14" cy="906.47" r="194.71" />
-            <circle className="cls-3" cx="966.45" cy="590.39" r="433.04" />
-            <circle className="cls-3" cx="968.35" cy="590.39" r="248.74" />
-        </g>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1940.07 1172.58">
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1">
+      <g>
+        <circle className="cls-1" cx="625.14" cy="288.54" r="194.71"/>
+        <circle className="cls-1" cx="1307.74" cy="288.54" r="194.71"/>
+        <circle className="cls-2" cx="966.44" cy="107.13" r="107.13"/>
+        <circle className="cls-2" cx="966.44" cy="250.05" r="33.85"/>
+        <circle className="cls-2" cx="966.44" cy="1065.44" r="107.13"/>
+        <circle className="cls-2" cx="966.44" cy="922.53" r="33.85"/>
+        <circle className="cls-2" cx="1443.51" cy="590.37" r="107.13"/>
+        <circle className="cls-2" cx="1300.59" cy="590.39" r="33.85"/>
+        <circle className="cls-2" cx="496.56" cy="590.39" r="107.13"/>
+        <circle className="cls-2" cx="639.47" cy="590.39" r="33.85"/>
+        <circle className="cls-1" cx="1307.74" cy="906.47" r="194.71"/>
+        <circle className="cls-1" cx="1745.35" cy="590.37" r="194.71"/>
+        <circle className="cls-1" cx="194.71" cy="590.39" r="194.71"/>
+        <circle className="cls-1" cx="625.14" cy="906.47" r="194.71"/>
+        <circle className="cls-3" cx="966.44" cy="590.39" r="433.04"/>
+        <circle className="cls-3" cx="968.34" cy="590.39" r="248.74"/>
+      </g>
     </g>
+  </g>
 </svg>
 </div>
 
