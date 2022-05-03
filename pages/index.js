1dff298c66fb1b7447b1847f4d5d656b1f0649ea
ritughiya <ritughiya@gmail.com>
2022-05-03 16:41:33 -0400

    changes to mobile

--- pages/index.js
+++ pages/index.js
@@ -84,12 +84,14 @@ const IndexPage = ({ properties }) => {
         
         } else {
           return (
-            <><div className="vimeo-wrapper pointer desktop">
-              <iframe loading="lazy" src="https://player.vimeo.com/video/675559033?h=8b71bc1ada&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>
+            <><div className="vimeo-wrapper pointer ">
+              <div className="img mobile">
+                          <Image className="mobile" src={Spaceshipoverlay} /></div>
+              <iframe loading="lazy" className="desktop" src="https://player.vimeo.com/video/675559033?h=8b71bc1ada&amp;autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0&amp;controls=0&amp;autopause=false?background=1&amp;muted=1" width="640" height="360" frameBorder="0" allowFullScreen></iframe>
             </div>
-            <div className="vimeo-wrapper pointer mobile">
+            {/* <div className="vimeo-wrapper pointer mobile">
             <Image src={Spaceshipoverlay} />
-            </div>
+            </div> */}
             
             <Animation3 />
             
