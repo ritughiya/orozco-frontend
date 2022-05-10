2e45df4b8f44b875c38dccef4dba034c5ef26a44
ritughiya <ritughiya@gmail.com>
2022-05-10 15:35:17 -0700

    translate

--- components/Customhead.js
+++ components/Customhead.js
@@ -28,7 +28,7 @@
 <meta property="og:url" content="https://go-spacetime.com/" />
 <meta property="og:description" 
   content="Go-Spacetime is the online extension of Mexican artist, Gabriel Orozco’s (b. 1962), Spacetime exhibition, which opened at 24 West 57th Street, Room 305, New York, NY in June 2021. An open-archive project, the exhibition consists of a selection of over 500 works in a wide variety of media, including sculpture, drawing, photography and painting.  " />
-
+  <meta name="google" content="notranslate" />
       </Head>
       
    );
