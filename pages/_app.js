72f28200e08f0eb92e598d98f2937b8ad22d5563
ritughiya <ritughiya@gmail.com>
2022-05-10 22:20:46 -0700

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import '../styles/globals.css'
+import Script from 'next/script'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-CR253G8DRR`}
+      />
+  
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-CR253G8DRR');
+        `}
+      </Script>
+      
+  
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
+
+
