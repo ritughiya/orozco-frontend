bbc786dac5e49f57f2ffd3d76390cb4fc123e00a
ritughiya <ritughiya@gmail.com>
2022-05-10 22:22:43 -0700

    fix script

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,12 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
+      <Script id="analytics1"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-CR253G8DRR`}
       />
   
-      <Script strategy="lazyOnload">
+      <Script id="analytics2" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
