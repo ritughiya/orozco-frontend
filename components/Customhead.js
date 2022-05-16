9d1f8de618bdf66a02111beab3ba9a2a654bf160
ritughiya <ritughiya@gmail.com>
2022-05-16 11:24:03 -0700

    share image

--- components/Customhead.js
+++ components/Customhead.js
@@ -28,7 +28,7 @@
         <link rel="icon" href="/favicon.png" />
         <meta property="og:title" content="Spacetime Library" />
 <meta property="og:url" content="https://go-spacetime.com/" />
-<meta property="og:image" href="/spacetime.png" />
+<meta property="og:image" content="https://go-spacetime.com/spacetime.png" />
 <meta property="og:type" content="website" />
 <meta property="og:locale" content="en_US" />
 <meta property="og:description" 
@@ -38,7 +38,7 @@
   <meta name="twitter:site" content="@MarianGoodman" />
   <meta name="twitter:title" content="Gabriel Orozco: Spacetime" />
 <meta name="twitter:description" content="GO-Spacetime is the online extension of Mexican artist, Gabriel Orozco’s (b. 1962), Spacetime exhibition, which opened at 24 West 57th Street, Room 305, New York, NY in June 2021. An open-archive project, the exhibition consists of a selection of over 500 works in a wide variety of media, including sculpture, drawing, photography and painting." />
-<meta name="twitter:image" content="/spacetime.png" />
+<meta name="twitter:image" content="https://go-spacetime.com/spacetime.png" />
 
 
 
