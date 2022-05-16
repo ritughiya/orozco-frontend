dfbacc1241dcf9bf2f8771653cc79cfa925eb91b
ritughiya <ritughiya@gmail.com>
2022-05-16 10:32:22 -0700

    twitter additons

--- components/Customhead.js
+++ components/Customhead.js
@@ -34,11 +34,11 @@
 <meta property="og:description" 
   content="GO-Spacetime is the online extension of Mexican artist, Gabriel Orozco’s (b. 1962), Spacetime exhibition, which opened at 24 West 57th Street, Room 305, New York, NY in June 2021. An open-archive project, the exhibition consists of a selection of over 500 works in a wide variety of media, including sculpture, drawing, photography and painting.  " />
   <meta name="google" content="notranslate" />
-  <meta name="twitter:title" content="Gabriel Orozco: Spacetime"></meta>
-<meta name="twitter:description" content="GO-Spacetime is the online extension of Mexican artist, Gabriel Orozco’s (b. 1962), Spacetime exhibition, which opened at 24 West 57th Street, Room 305, New York, NY in June 2021. An open-archive project, the exhibition consists of a selection of over 500 works in a wide variety of media, including sculpture, drawing, photography and painting."></meta>
-<meta name="twitter:image" content="/spacetime.png"></meta>
-<meta name="twitter:card" content="summary_large_image"></meta>
-<meta name="twitter:site" content="@MarianGoodman"></meta>
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@MarianGoodman" />
+  <meta name="twitter:title" content="Gabriel Orozco: Spacetime" />
+<meta name="twitter:description" content="GO-Spacetime is the online extension of Mexican artist, Gabriel Orozco’s (b. 1962), Spacetime exhibition, which opened at 24 West 57th Street, Room 305, New York, NY in June 2021. An open-archive project, the exhibition consists of a selection of over 500 works in a wide variety of media, including sculpture, drawing, photography and painting." />
+<meta name="twitter:image" content="/spacetime.png" />
 
 
 
