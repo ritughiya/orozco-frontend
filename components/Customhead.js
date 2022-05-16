93e0eddfb589f98d1ee396da43051eedca31584b
ritughiya <ritughiya@gmail.com>
2022-05-16 10:28:29 -0700

    fb img

--- components/Customhead.js
+++ components/Customhead.js
@@ -28,8 +28,7 @@
         <link rel="icon" href="/favicon.png" />
         <meta property="og:title" content="Spacetime Library" />
 <meta property="og:url" content="https://go-spacetime.com/" />
-<meta property="og:url" content="https://go-spacetime.com/" />
-<meta property="og:image" content="/spacetime.png" />
+<meta property="og:image" href="/spacetime.png" />
 <meta property="og:type" content="website" />
 <meta property="og:locale" content="en_US" />
 <meta property="og:description" 
